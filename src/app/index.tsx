fdcfe61b28b7fe76ac870578d9e369e5386b59ef
Rushikesh <rushimangore36@gmail.com>
2026-09-07 19:12:36 +0530

    UI: change home page to minimal

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,10 +3,8 @@ import "../../global.css";
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
-        Welcome to Nativewind!
-      </Text>
+    <View className="flex-1 bg-white px-6 pt-16">
+      <Text className="text-3xl font-bold text-black">Available Devices</Text>
     </View>
   );
 }
