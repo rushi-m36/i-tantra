cf33eb60ee8e53b2387a05caf2204d862b51cbea
Rushikesh Mangore <rushimangore36@gmail.com>
2026-09-11 18:30:40 +0530

    Polish production app lifecycle and logging

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,9 +1,12 @@
 import { Stack } from "expo-router";
+import "../utils/production";
 import { CommunicationProvider } from "../context/CommunicationContext";
+import { DiscoveryLifecycle } from "../components/DiscoveryLifecycle";
 
 export default function RootLayout() {
   return (
     <CommunicationProvider>
+      <DiscoveryLifecycle />
       <Stack screenOptions={{ headerShown: false }} />
     </CommunicationProvider>
   );
