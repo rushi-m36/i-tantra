2a636b329bef48c20b7824f95c87c001795de139
Rushikesh Mangore <rushimangore36@gmail.com>
2026-09-13 09:31:57 +0530

    Register dedicated NSD package

--- android/app/src/main/java/com/rushi36/itantra/MainApplication.kt
+++ android/app/src/main/java/com/rushi36/itantra/MainApplication.kt
@@ -21,7 +21,7 @@ class MainApplication : Application(), ReactApplication {
       context = applicationContext,
       packageList =
         PackageList(this).packages.apply {
-          add(LocalNetworkPackage())
+          add(NsdDiscoveryPackage())
         }
     )
   }
