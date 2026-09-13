1b439e4241670e9f3d9657b8b8edb6c4e9579e20
Rushikesh Mangore <rushimangore36@gmail.com>
2026-09-13 09:30:22 +0530

    Use only native NSD discovery

--- android/app/src/main/java/com/rushi36/itantra/LocalNetworkPackage.kt
+++ android/app/src/main/java/com/rushi36/itantra/LocalNetworkPackage.kt
@@ -1,6 +1,5 @@
 package com.rushi36.itantra
 
-import com.facebook.react.PackageList
 import com.facebook.react.ReactPackage
 import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
@@ -8,7 +7,7 @@ import com.facebook.react.uimanager.ViewManager
 
 class LocalNetworkPackage : ReactPackage {
   override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> =
-    listOf(LocalNetworkModule(reactContext), NsdDiscoveryModule(reactContext))
+    listOf(NsdDiscoveryModule(reactContext))
 
   override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> =
     emptyList()
